2de57a0db1bec442ecd4fd52f8cfe6db62a8276e
myy <myy@qq.com>
2019-01-08 22:03:47 +0800

    拆分爬虫类

--- fanyi/Crawler.php
+++ fanyi/Crawler.php
@@ -11,5 +11,244 @@ namespace Fanyi;
 
 class Crawler
 {
+    private $curl;
+    private $proxyHost = '127.0.0.1';//代理服务器的ip
+    private $proxyPort = '8888';//代理服务器的端口
+    private $timeoutLimit = 30;
+    protected $cookiePath = './cookie/';
+    private $cookieFile = '';
 
+    private $open_proxy = true;
+
+    public function __construct()
+    {
+        $this->curl = curl_init();
+        $this->cookieFile = $this->cookieFile . 'cookie.log';
+    }
+
+    /**
+     * @param string $proxyHost
+     */
+    public function setProxyHost($proxyHost)
+    {
+        $this->proxyHost = $proxyHost;
+    }
+
+    /**
+     * @param string $proxyPort
+     */
+    public function setProxyPort($proxyPort)
+    {
+        $this->proxyPort = $proxyPort;
+    }
+
+    /**
+     * @param int $timeoutLimit
+     */
+    public function setTimeoutLimit($timeoutLimit)
+    {
+        $this->timeoutLimit = $timeoutLimit;
+    }
+
+    /**
+     * @param string $cookiePath
+     */
+    public function setCookiePath($cookiePath)
+    {
+        $this->cookiePath = $cookiePath;
+    }
+
+    /**
+     * @param boolean $open_proxy
+     */
+    public function setOpenProxy($open_proxy)
+    {
+        $this->open_proxy = $open_proxy;
+    }
+
+    /**
+     * 指定cookie文件名
+     * @param string $cookieFile cookie文件名
+     * @return void
+     */
+    public function setCookieFile($cookieFile)
+    {
+        $this->cookieFile = $this->cookieath . $cookieFile;
+    }
+
+    /**
+     * @return int
+     */
+    public function getTimeoutLimit()
+    {
+        return $this->timeoutLimit;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCookiePath()
+    {
+        return $this->cookiePath;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isOpenProxy()
+    {
+        return $this->openProxy;
+    }
+
+
+    /**
+     * 执行get请求
+     *
+     * @param $url
+     * @param bool $need_follow
+     * @param array $header_set
+     * @param bool $need_header
+     * @return string
+     */
+    public function doGetRequest($url, $need_follow = true, $header_set = [], $need_header = false) {
+        curl_reset($this->curl);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POST, 0);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeoutLimit);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_COOKIEJAR,$this->cookieFile); //保存cookie到指定文件
+        curl_setopt($this->curl, CURLOPT_COOKIEFILE,$this->cookieFile); //使用cookie文件
+
+        //HTTPS
+        $this->setHttpsOpt();
+
+        if (!empty($header_set)) {
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $header_set);//设置请求头
+
+            foreach ($header_set as $v) {
+                if ($v == 'Accept-Encoding: gzip, deflate') {
+                    curl_setopt($this->curl, CURLOPT_ENCODING,'gzip');
+                }
+            }
+        }
+
+        if ($need_follow) {
+            curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true); //支持重定向
+            curl_setopt($this->curl, CURLOPT_MAXREDIRS, 20);//最大跳转数
+        }
+
+        if ($need_header) {
+            curl_setopt($this->curl, CURLOPT_HEADER, true);//需要获取返回头信息
+        }
+
+        //代理服务器地址
+        $this->setCurlProxy();
+
+        $res_page = $this->execCurl();
+
+        return $res_page;
+    }
+
+    /**
+     * 设置代理
+     */
+    private function setCurlProxy() {
+        if ($this->open_proxy) {
+            curl_setopt($this->curl, CURLOPT_PROXY, $this->proxyHost);
+            curl_setopt($this->curl, CURLOPT_PROXYPORT, $this->proxyPort);
+        }
+    }
+
+    /**
+     *  执行curl
+     *
+     * @return mixed
+     * @throws Exception
+     */
+    private function execCurl()
+    {
+        $res = curl_exec($this->curl);
+        if (curl_errno($this->curl)) {
+            echo 'Curl error: ' . curl_errno($this->curl) . '----' . curl_error($this->curl) . PHP_EOL;
+            return false;
+        }
+
+        return $res;
+    }
+
+    /**
+     * 获取通用请求头
+     *
+     * @param $host
+     * @param array $headerExt
+     * @return array
+     */
+    public function getNormalHeader($host, array $headerExt = [])
+    {
+        $header = [
+            'Host: ' . $host,
+            'Pragma: no-cache',
+            'Accept: */*',
+            'Accept-Language: en-US,zh-CN;q=0.7,en-GB;q=0.3',
+            'User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)',
+            'Connection: Keep-Alive',
+        ];
+
+        $header = array_merge($header, $headerExt);
+
+        return $header;
+    }
+
+
+    /**
+     * 执行post请求
+     *
+     * @param string $url 请求url
+     * @param array $header 指定请求头
+     * @param string $post_data (id=1&name=asf) post表单字段
+     * @param boolean $need_header 是否需要响应头
+     * @param boolean $use_cookie 是否需要使用cookie
+     * @return string
+     */
+    public function doPostRequest($url, $header = [], $post_data = '', $need_header = false, $use_cookie = true)
+    {
+        curl_reset($this->curl);
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_POST, 1);
+        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeoutLimit);
+
+        if (!empty($header)) {
+            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $header);//设置请求头
+        }
+
+        if (!empty($post_data)) {
+            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $post_data); //指定请求行请求参数
+        }
+
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($this->curl, CURLOPT_COOKIEJAR,$this->cookieFile); //保存cookie到指定文件
+
+        if ($use_cookie) {
+            curl_setopt($this->curl, CURLOPT_COOKIEFILE,$this->cookieFile); //使用cookie文件
+        }
+
+        $this->setHttpsOpt();
+
+        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, false); //取消重定向
+
+        if ($need_header) {
+            curl_setopt($this->curl, CURLOPT_HEADER, true);//需要获取返回头信息
+        }
+
+        $this->setCurlProxy();
+        $res = $this->execCurl();
+        return $res;
+    }
+
+    private function setHttpsOpt()
+    {
+        //HTTPS
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0); // 跳过证书检查
+        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 2);  // 从证书中检查SSL加密算法是否存在
+    }
 }
--- fanyi/Translate.php
+++ fanyi/Translate.php
@@ -3,21 +3,18 @@
 namespace Fanyi;
 use \Exception;
 use \SplDoublyLinkedList;
+use Fanyi\Crawler;
 
 class Translate {
-    private $curl;
+    private $crawler;
     private $url = 'https://fanyi.baidu.com';
     private $host = 'fanyi.baidu.com';
-    private $open_proxy = true;
-    private $timeout_limit = 30;
+
     private $filePath = '';//资源文件所在位置
-    private $cookie_file = '';
-    private $proxy_host = '192.168.0.109';//代理服务器的ip
-    private $proxy_port = '8888';//代理服务器的端口
+
     protected $phantomjs_bin_path = "D:\\app\\phantomjs\\bin";//phantomjs所在绝对路径，用来解析js
     protected $resource_path = './resource/';
     protected $output_path = './output/';
-    protected $cookie_path = './cookie/';
 
     const PHP_LANG_FILE = 1;//tp的lang语言包类型，key 为 中文， val 为英文
     const JAVA_LANG_FILE = 2;//java的lang语言包类型
@@ -27,22 +24,15 @@ class Translate {
 
     public function __construct()
     {
-        $this->curl = curl_init();
-        $this->cookie_file = $this->cookie_path . 'cookie';
         $this->lang_file_type = self::PHP_LANG_FILE;
         $this->resouce_data = new ResourceData();
+        $this->crawler = new Crawler();
+        $this->crawler->setOpenProxy(false);
+        $this->crawler->setProxyHost('192.168.0.109');
+        $this->crawler->setProxyPort('8888');
     }
 
-    /**
-     * 指定cookie文件名
-     * @param string $cookieFile cookie文件名
-     * @return resource|string
-     */
-    public function setCookieFile($cookieFile)
-    {
-        $this->cookie_file = $this->cookie_path . $cookieFile;
-        return $this;
-    }
+
 
     /**
      * @param string $resource_file 应该放在 ./resource 目录下
@@ -90,7 +80,7 @@ class Translate {
     /**
      * 从Tp 的lang 包中提取中英文信息：为了保留注释内容而没有直接使用数组方式
      * @param bool $key_is_cn key内容为中文
-     * @return
+     * @return void
      */
     public function getDataFromTPLangFile($key_is_cn = true)
     {
@@ -132,158 +122,6 @@ class Translate {
         }
     }
 
-    /**
-     * 执行get请求
-     *
-     * @param $url
-     * @param bool $need_follow
-     * @param array $header_set
-     * @param bool $need_header
-     * @return string
-     */
-    protected function doGetRequest($url, $need_follow = true, $header_set = [], $need_header = false) {
-        curl_reset($this->curl);
-        curl_setopt($this->curl, CURLOPT_URL, $url);
-        curl_setopt($this->curl, CURLOPT_POST, 0);
-        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeout_limit);
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->curl, CURLOPT_COOKIEJAR,$this->cookie_file); //保存cookie到指定文件
-        curl_setopt($this->curl, CURLOPT_COOKIEFILE,$this->cookie_file); //使用cookie文件
-
-        //HTTPS
-        $this->setHttpsOpt();
-
-        if (!empty($header_set)) {
-            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $header_set);//设置请求头
-
-            foreach ($header_set as $v) {
-                if ($v == 'Accept-Encoding: gzip, deflate') {
-                    curl_setopt($this->curl, CURLOPT_ENCODING,'gzip');
-                }
-            }
-        }
-
-        if ($need_follow) {
-            curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, true); //支持重定向
-            curl_setopt($this->curl, CURLOPT_MAXREDIRS, 20);//最大跳转数
-        }
-
-        if ($need_header) {
-            curl_setopt($this->curl, CURLOPT_HEADER, true);//需要获取返回头信息
-        }
-
-        //代理服务器地址
-        $this->setCurlProxy();
-
-        $res_page = $this->execCurl();
-
-        return $res_page;
-    }
-
-    /**
-     * 设置代理
-     */
-    private function setCurlProxy() {
-        if ($this->open_proxy) {
-            curl_setopt($this->curl, CURLOPT_PROXY, $this->proxy_host);
-            curl_setopt($this->curl, CURLOPT_PROXYPORT, $this->proxy_port);
-        }
-    }
-
-    /**
-     *  执行curl
-     *
-     * @return mixed
-     * @throws Exception
-     */
-    private function execCurl()
-    {
-        $res = curl_exec($this->curl);
-        if (curl_errno($this->curl)) {
-            echo 'Curl error: ' . curl_errno($this->curl) . '----' . curl_error($this->curl) . PHP_EOL;
-            return false;
-        }
-
-        return $res;
-    }
-
-    /**
-     * 获取通用请求头
-     *
-     * @param $host
-     * @param array $headerExt
-     * @return array
-     */
-    private function getNormalHeader($host, array $headerExt = [])
-    {
-        $header = [
-            'Host: ' . $host,
-            'Pragma: no-cache',
-            'Accept: */*',
-            'Accept-Language: en-US,zh-CN;q=0.7,en-GB;q=0.3',
-            'User-Agent: Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Win64; x64; Trident/5.0)',
-            'Connection: Keep-Alive',
-        ];
-
-        $header = array_merge($header, $headerExt);
-
-        return $header;
-    }
-
-
-    /**
-     * 执行post请求
-     *
-     * @param string $url 请求url
-     * @param array $header 指定请求头
-     * @param string $post_data (id=1&name=asf) post表单字段
-     * @param boolean $need_header 是否需要响应头
-     * @param boolean $use_cookie 是否需要使用cookie
-     * @return string
-     */
-    protected function doPostRequest($url, $header = [], $post_data = '', $need_header = false, $use_cookie = true)
-    {
-        curl_reset($this->curl);
-        curl_setopt($this->curl, CURLOPT_URL, $url);
-        curl_setopt($this->curl, CURLOPT_POST, 1);
-        curl_setopt($this->curl, CURLOPT_TIMEOUT, $this->timeout_limit);
-
-        if (!empty($header)) {
-            curl_setopt($this->curl, CURLOPT_HTTPHEADER, $header);//设置请求头
-        }
-
-        if (!empty($post_data)) {
-            curl_setopt($this->curl, CURLOPT_POSTFIELDS, $post_data); //指定请求行请求参数
-        }
-
-        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($this->curl, CURLOPT_COOKIEJAR,$this->cookie_file); //保存cookie到指定文件
-
-        if ($use_cookie) {
-            curl_setopt($this->curl, CURLOPT_COOKIEFILE,$this->cookie_file); //使用cookie文件
-        }
-
-        $this->setHttpsOpt();
-
-        curl_setopt($this->curl, CURLOPT_FOLLOWLOCATION, false); //取消重定向
-
-        if ($need_header) {
-            curl_setopt($this->curl, CURLOPT_HEADER, true);//需要获取返回头信息
-        }
-
-        $this->setCurlProxy();
-        $res = $this->execCurl();
-        return $res;
-    }
-
-    private function setHttpsOpt()
-    {
-        //HTTPS
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYPEER, 0); // 跳过证书检查
-        curl_setopt($this->curl, CURLOPT_SSL_VERIFYHOST, 2);  // 从证书中检查SSL加密算法是否存在
-    }
-
-
     protected function encryptSign($query, $gtk)
     {
 
@@ -312,7 +150,6 @@ class Translate {
 
     }
 
-    //在cli下输入数据
     private function _printMsg($msg) {
         echo $msg.PHP_EOL;
     }
@@ -331,7 +168,7 @@ class Translate {
         if ($total == 0) exit($this->_printMsg('have no data'));
 
         //第一次请求:/ 获取 token、gtk参数 以及生成相应的cookie
-        $res1 = $this->doGetRequest($this->url, true, $this->getNormalHeader($this->host, [
+        $res1 = $this->crawler->doGetRequest($this->url, true, $this->crawler->getNormalHeader($this->host, [
             'Cache-Control: max-age=0',
             'Upgrade-Insecure-Requests: 1',
             'SocketLog: SocketLog(tabid=1261&client_id=)',
@@ -354,7 +191,7 @@ class Translate {
         //模拟第二次请求：langdetect
         $url2 = $this->url . '/langdetect';
         $post_data = http_build_query(['query' => '你好']);
-        $this->doPostRequest($url2, $this->getNormalHeader($this->host, [
+        $this->crawler->doPostRequest($url2, $this->crawler->getNormalHeader($this->host, [
             'Content-Length: ' . strlen($post_data),
             'Upgrade-Insecure-Requests: 1',
             'Origin: ' . $this->url,
@@ -373,8 +210,7 @@ class Translate {
                 //没有待替换的内容，则不做任何处理，原样输出即可
                 fwrite($fp, $data_meta->getOrign());
             } else {
-                //如果en已经有值了，也不做任何处理
-
+                //如果en已经有值了，也不做任何处理(由于已经在生产资源集时已经处理了，这里就不再处理了)
                 $query_origin = $data_meta->getCn();
                 $query = mb_convert_encoding($query_origin, 'GBK', 'UTF-8');
                 //生成 sign 参数
@@ -398,7 +234,7 @@ class Translate {
                     'token' => $token,
                 ]);
 
-                $res3 = $this->doPostRequest($url3, $this->getNormalHeader($this->host, [
+                $res3 = $this->crawler->doPostRequest($url3, $this->crawler->getNormalHeader($this->host, [
                     'Content-Length: ' . strlen($post_data),
                     'Origin: ' . $this->url,
                     'X-Requested-With: XMLHttpRequest',
